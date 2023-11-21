0bf422e063db3b41b2d5ce18c2c49a7c49296266
Filip-D91 <filip.dropuljic@yahoo.com>
2023-11-21 19:46:08 +0000

    added demo files

---
+++ demo1.py
@@ -0,0 +1,10 @@
+print('Dobro dosli u PyDev Program obrazovanja')
+
+first_name = 'Pero'
+last_name = 'Peric'
+date_of_birth = 1998
+mobile_phone = '+385 9n 1234 567'
+employed = True
+height = 180.99
+
+print(first_name, last_name)
