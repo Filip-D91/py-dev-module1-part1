8fda55a480cde7a116e3fa5d6177e4012df364ac
Filip-D91 <filip.dropuljic@yahoo.com>
2023-11-23 20:38:13 +0000

    print excercise

---
+++ print_demo.py
@@ -0,0 +1,12 @@
+#help(print)
+
+first_name = 'Pero'
+last_name = 'Peric'
+phone = '+385 9_ 1234 567'
+
+#print()
+#print(first_name, end = ' ')
+#print(first_name, last_name, phone)
+#print(first_name, last_name, phone, sep = ', ')
+
+#print(first_name, last_name, phone, sep = ', ')
